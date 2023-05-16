cae7c9af7bfda15abaa1334e877cd615c28e3285
Jameszzyyyyy <97641352+Jameszzyyyyy@users.noreply.github.com>
2023-05-15 17:06:48 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -48,8 +48,8 @@ function initializeServiceWorker() {
   if('serviceWorker' in navigator){
     window.addEventListener('load', async function(event){
       try {
-        const registration = await navigator.serviceWorker.register("/sw.js", {
-          scope: "/",
+        const registration = await navigator.serviceWorker.register("./sw.js", {
+          scope: "./",
         });
         if (registration.active) {
           console.log("Service worker active");
